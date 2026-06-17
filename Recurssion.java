6552460009dada4937cb09b4d5f107abed87c0d8
Daveram1714 <daveram4529a@gmail.com>
2026-06-17 06:24:59 +0530

    print 1 to n also n to 1 backtrackking

--- Recurssion.java
+++ Recurssion.java
@@ -12,11 +12,39 @@ public class Recurssion {
     }
     
 
+    public void Resurssion2(int n){
+        if(n ==0) return;
+        else{
+            System.out.print(n);
+            Resurssion2(n-1);
+        }
+    }
+
+    public void Resurssion3(int i,int n){
+        if(i < 1 ) return; 
+   
+            Resurssion3(i-1,n);
+            System.out.print(i);
+    
+    }
+
+//     public void Resurssion3(int i, int n) {
+// // Base case: stop when i is less than 1
+// if (i < 1) return;
+
+// // Recursive call first
+// Resurssion3(i - 1, n);
+
+// // Print after the call (backtracking)
+// System.out.print(i + " ");
+// }
     public static void main(String[]args){
         Scanner sc  = new Scanner(System.in);
         System.out.print("Enter the number of time to Print : ");
         int N = sc.nextInt();
         Recurssion rs = new Recurssion();
-        rs.Recurssion1(N);
+        // rs.Recurssion1(N);
+        // rs.Resurssion2(N);
+        rs.Resurssion3(N,N);
     }
 }

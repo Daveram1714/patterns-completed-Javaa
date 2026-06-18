1d1385b01a8c22b190f56176b9bb7a4fa74b1ba7
Daveram1714 <daveram4529a@gmail.com>
2026-06-18 06:45:22 +0530

    recurssion factorials N to 1 and Sum of First numbers

--- Recurssion.java
+++ Recurssion.java
@@ -28,16 +28,43 @@ public class Recurssion {
     
     }
 
-//     public void Resurssion3(int i, int n) {
-// // Base case: stop when i is less than 1
-// if (i < 1) return;
 
-// // Recursive call first
-// Resurssion3(i - 1, n);
+    public void Recurssion4(int i,int n){
+        if(i>n) return;
+        Recurssion4(i+1, n);
+        System.out.print(i);
+    }
 
-// // Print after the call (backtracking)
-// System.out.print(i + " ");
-// }
+    public void Recurssion4Forward(int n){
+        if( n ==0) return;;
+        System.out.println(n);
+        Recurssion4Forward(n-1);
+    }
+
+ 
+    public void SumofTwoParameters(int n,int sum){
+         if(n < 1) {
+            System.out.println(sum);
+            return;
+        }
+        SumofTwoParameters(n-1, sum+n);
+    }
+
+
+    public int SumOfTwoFunctional(int n){
+        if(n == 0 ){
+            return 0 ;
+        }
+        return  n + SumOfTwoFunctional(n-1);
+
+    }
+   
+    public int FactoralOfNumber(int n){
+        if(n <1 ) return  1;
+        return  n * FactoralOfNumber(n-1);
+    }
+   
+
     public static void main(String[]args){
         Scanner sc  = new Scanner(System.in);
         System.out.print("Enter the number of time to Print : ");
@@ -45,6 +72,12 @@ public class Recurssion {
         Recurssion rs = new Recurssion();
         // rs.Recurssion1(N);
         // rs.Resurssion2(N);
-        rs.Resurssion3(N,N);
+        // rs.Resurssion3(N,N);
+        // rs.Recurssion4(0,N);
+        // rs.Recurssion4Forward(N);
+        // rs.SumofTwoParameters(N,0);
+        // int result = rs.SumOfTwoFunctional(N);
+        int result = rs.FactoralOfNumber(N);
+        System.out.println("Result " + result);
     }
 }

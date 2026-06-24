e666d274352595378796c0876a7ad3925f43f973
Daveram1714 <daveram4529a@gmail.com>
2026-06-24 06:38:35 +0530

    started learning Hashing and Now completed preloading

---
+++ Hashing.java
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;
+
+public class Hashing {
+    
+    public static void main(String[] args) {
+        int n;
+        System.out.println("Enter the size of the array");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int []arr = new int[n];
+        for(int i = 0 ;i<n;i++){
+                arr[i] = sc.nextInt();
+        }
+
+
+
+        int hash[] = new int[13];
+
+        for (int i = 0; i < n; i++) {
+                hash[arr[i]] += 1; 
+        }
+
+
+        int q;
+        System.out.println("Enter the Q");
+        q = sc.nextInt();
+
+        while(q-- >0){
+            int number = sc.nextInt();
+            System.out.println("Hash Number" + hash[number]);
+        }
+
+        
+        
+    }
+    
+    // return 0 ;  
+
+
+
+}

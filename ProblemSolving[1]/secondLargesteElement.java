7ef41e69f2d87c6c081fd331682e1271332e96b1
Daveram1714 <daveram4529a@gmail.com>
2026-07-21 06:16:13 +0530

    second smallest element

--- ProblemSolving[1]/secondLargesteElement.java
+++ ProblemSolving[1]/secondLargesteElement.java
@@ -38,11 +38,51 @@ class secondLargestElement{
         System.out.println("The Second Largest is " + sLargest);
 
     }
+
+    public void optimal(int arr[]){
+        int largest = Integer.MIN_VALUE, slargest = Integer.MIN_VALUE;
+
+        for(int i = 0 ; i<arr.length ; i++){
+            if(arr[i] > largest){
+                slargest = largest;
+                largest = arr[i];
+            }
+            else {
+                if(arr[i] < largest && arr[i] > slargest){
+                    slargest = arr[i];
+                }
+            }
+        }
+        System.out.println("Second Largest : "+ slargest);
+    }
+
+
+    public  void optimalSecondsmall(int[] arr) {
+        int small = Integer.MAX_VALUE , second_small = Integer.MAX_VALUE;
+    
+        
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] < small ){
+                second_small = small;
+                small = arr[i];
+            }
+            else {
+                if( arr[i] < second_small && arr[i] != small){
+                    second_small = arr[i];
+                }
+            }
+        }
+
+        System.err.println("Second small" + second_small);
+
+}
     
     public static void main(String[] args) {
 
         secondLargestElement se = new secondLargestElement();
         int arr[] = {6,1,2,4,7,7,5};
-        se.brute(arr);
+        // se.brute(arr);
+        se.optimal(arr);
+        se.optimalSecondsmall(arr);
     }
 }

d956beb07ea8d7043e92496e6db41604202ccf0a
Daveram1714 <daveram4529a@gmail.com>
2026-07-31 06:39:41 +0530

    longest Sub arraay brute completed

---
+++ ProblemSolving[1]/longestSubarray.java
@@ -0,0 +1,51 @@
+
+import java.util.Scanner;
+
+class longestSubarray{
+
+
+    public int Brute(int n,int k  ,int nums[]){
+        int maxLength = 0;
+        // for (int i = 0; i < n; i++) {
+        //     long s = 0;
+        //         for (int j = i; j < n; j++) {
+        //                 s+=nums[j];
+  
+        //             if(s ==k){
+        //                 maxLength = Math.max(maxLength, j-i+1);
+        //             }
+        //         }
+        // }
+        
+        // Better version (O(N^2))
+for (int i = 0; i < n; i++) {
+    long s = 0; // Start sum from i
+    for (int j = i; j < n; j++) {
+        s += nums[j]; // Just add the next element, no need for loop 'm'
+        if (s == k) {
+            maxLength = Math.max(maxLength, j - i + 1);
+        }
+    }
+}
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Size of the array : ");
+        int n = sc.nextInt();
+        System.out.print("Enter the Sum  of the k : ");
+        int k = sc.nextInt();
+        System.out.print("Enter the  the array Elements : ");
+        int nums []= new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        sc.close();
+        longestSubarray ls = new  longestSubarray();
+        System.out.println("The value is  : "+ls.Brute(n, k, nums));
+
+    }
+
+
+}

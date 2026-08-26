a8a14866e3b5b5a85abed035883a4f22a383d01e
Daveram1714 <daveram4529a@gmail.com>
2026-08-26 05:54:06 +0530

    Majority of 2 element optimal completed

--- HardProblemSolving/MajorityElement2.java
+++ HardProblemSolving/MajorityElement2.java
@@ -36,15 +36,58 @@ class MajorityElement2 {
         Map<Integer, Integer> mpp = new HashMap<>();
         int mini = n / 3 + 1;
         for (int i = 0; i < n; i++) {
-            mpp.put(arr[i] , mpp.getOrDefault(arr[i],0 )+1 );
+            mpp.put(arr[i], mpp.getOrDefault(arr[i], 0) + 1);
 
-            if(mpp.get(arr[i]) == mini ){
+            if (mpp.get(arr[i]) == mini) {
                 result.add(arr[i]);
             }
-            if(result.size()==  2) break;
+            if (result.size() == 2)
+                break;
         }
 
         return result;
+    }
+
+    public List<Integer> optimal(int arr[]) {
+        List<Integer> result = new ArrayList<>();
+        int cnt1 = 0, cnt2 = 0;
+        int ele1 = Integer.MIN_VALUE, ele2 = Integer.MIN_VALUE;
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            if (cnt1 == 0 &&  ele2 != arr[i]) {
+                cnt1 = 1;
+                ele1 = arr[i];
+            }else if(cnt2 == 0 && ele1 != arr[i]){
+                cnt2 =1 ; 
+                ele2 = arr[i];
+            }else if(ele1 == arr[i]){
+                cnt1++;
+            }else if( ele2 == arr[i]){
+                cnt2 ++;
+            }else{
+                cnt1--;
+                cnt2--;
+            }
+
+        }
+
+
+
+            int mini = n/3+1;
+            cnt1 = 0 ; cnt2 = 0 ;
+
+            for(int i = 0 ;i<n;i++){
+                if(ele1 == arr[i]) cnt1++;
+                if(ele2 == arr[i]) cnt2++;
+            }
+
+            if(cnt1>= mini) result.add(ele1);
+            if(cnt2>= mini) result.add(ele2);
+
+
+        return  result;
+
     }
 
     public static void main(String[] args) {
@@ -52,7 +95,8 @@ class MajorityElement2 {
 
         MajorityElement2 sol = new MajorityElement2();
         // List<Integer> ans = sol.brute(arr);
-        List<Integer> ans = sol.better(arr);
+        // List<Integer> ans = sol.better(arr);
+        List<Integer> ans = sol.optimal(arr);
 
         System.out.print("The majority elements are: ");
         for (int it : ans) {
